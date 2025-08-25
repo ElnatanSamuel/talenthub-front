aa99da199a94772bcf4f5ae8b8c0d4973418405a
elnatansamuel25 <elnatan.samuel@eaglelionsystems.com>
2025-08-25 09:19:05 +0300

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import LoginClient from "./LoginClient";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function LoginPage() {
   return (
     <Suspense fallback={<div className="p-4">Loading…</div>}>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import RegisterClient from "./RegisterClient";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function RegisterPage() {
   return (
     <Suspense fallback={<div className="p-4">Loading…</div>}>
